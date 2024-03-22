f60ae551fe5eeed5fbb74fc79dba95959e13e9d1
DamianRavinduPeiris <drpeiris3@gmail.com>
2024-03-22 17:07:52 +0530

    Added functionality to input options when parsing the tokens.

--- src/main/java/com/damian/jwtparser/Main.java
+++ src/main/java/com/damian/jwtparser/Main.java
@@ -3,20 +3,40 @@ package com.damian.jwtparser;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 
+import java.util.Scanner;
+
 class Main {
-    public static void main(String[] args) {
-        String JWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3MTEwOTg4OTEsImV4cCI6MTc0MjYzNDg5MSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkRhbWlhbiIsIlN1cm5hbWUiOiJQZWlyaXMiLCJFbWFpbCI6ImhlbGxvQGRhbWlhbi5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.hWdzgpWyI0xjPsUcMaAJGpgrrnFik7MlK_9lFrSjrno";
-        String key = "supersecretkey";
+    public static void main(String[] args) throws InterruptedException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the JWT token: ");
+        String JWT = input.next();
+
+        System.out.println("Enter the secret key: ");
+        String key = input.next();
+        System.out.println("What you want to extract from the token? ");
+        String extract = input.next();
+
         Claims claims = parseTokens(JWT, key);
-        System.out.println("Here's the email from token : "+claims.get("email"));
+        if(claims.get(extract)==null){
+            System.out.println("The token does not contain the " + extract + " you are looking for.");
+            return;
+
+        }
+        System.out.println("Here's the " + extract + " from the  token : " + claims.get(extract));
 
     }
 
-    public static Claims parseTokens(String jwt, String key){
-        return Jwts.parser()
-                .setSigningKey(key.getBytes())
-                .build()
-                .parseClaimsJws(jwt).getBody();
+    public static Claims parseTokens(String jwt, String key) {
+        try {
+            return Jwts.parser().setSigningKey(key.getBytes()).build().parseClaimsJws(jwt).getBody();
+
+
+        } catch (Exception exception) {
+            System.out.println("An exception occurred while parsing the token. Please check the token and the secret key. : " + exception.getLocalizedMessage());
+            return null;
+
+        }
+
 
     }
 
